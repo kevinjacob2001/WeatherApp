4dfb985a9eb1395c5d25bcd4a5cc0f6922fff5df
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-10 20:17:59 +0530

    added files

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div>How are you</div>
+        )
+    }
+}
+
+export default Home;
