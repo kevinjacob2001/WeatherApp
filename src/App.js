be8cfb6f4c4c6ab6471746f0a9ff1e3ffc541d19
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-11 13:41:20 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -9,12 +9,41 @@ const API_KEY="ba83f1e9b9127f1c19738fc56ba4aa3e";
 
 class App extends Component {
 
+state={
+  country:undefined,
+  city:undefined,
+  temperature:undefined,
+  humidity:undefined,
+  description:undefined,
+  error:undefined
+
+}
+
+
+  
  getWeather=async (e)=>{
 e.preventDefault();
-  const response=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`)
+const city=e.target.elements.city.value;
+const country=e.target.elements.country.value;
+const response=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
 const data=await response.json();
-console.log(data);
 
+if(city&&country){
+this.setState({city:data.name})
+this.setState({country:data.sys.country})
+this.setState({temperature:data.main.temp})
+this.setState({humidity:data.main.humidity})
+this.setState({description:data.weather[0].description})
+this.setState({error:""})
+}
+else{
+  this.setState({city:undefined})
+this.setState({country:undefined})
+this.setState({temperature:undefined})
+this.setState({humidity:undefined})
+this.setState({description:undefined})
+this.setState({error:"please complete the input field"})
+}
 }
 
 
@@ -25,7 +54,14 @@ console.log(data);
     <div>
 <Title/>
 <Form getWeather={this.getWeather}/>
-<Weather/>
+<Weather 
+country={this.state.country} 
+city={this.state.city} 
+temperature={this.state.temperature} 
+humidity={this.state.humidity} 
+description={this.state.description}
+error={this.state.error}
+/>
     </div>
 
   );
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Component } from 'react';
 
 
-class Weather extends Component{
-    render(){
+function Weather(props){
         return(
             <div>
-               Weather Component
+                {props.country&&(<h1>Country code: {props.country}</h1>)}
+
+       {props.city&&(<h1>City {props.city}</h1>)}
+      {props.temperature&&( <h1>Temperature {props.temperature}</h1>)}
+       {props.humidity&&( <h1>Humidity {props.humidity}</h1>)}
+       {props.description&&( <h2>Description:{props.description}</h2>)}
+        {props.error&&(<h2>{props.error}</h2>)}
             </div>
         )
     }
-}
 
 export default Weather;
