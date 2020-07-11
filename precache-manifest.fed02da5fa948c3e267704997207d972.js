f6511a36cb990f1a1f1f80a333e5f083359d17c0
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-11 19:58:12 +0530

    Updates

---
+++ precache-manifest.fed02da5fa948c3e267704997207d972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1725a80c4a564a84ca0adf07e050eca7",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "194961d4d6e9f9b5567b",
+    "url": "/WeatherApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fabdbb5ace72ff89281",
+    "url": "/WeatherApp/static/css/main.593bb49d.chunk.css"
+  },
+  {
+    "revision": "194961d4d6e9f9b5567b",
+    "url": "/WeatherApp/static/js/2.5ce946c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.5ce946c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fabdbb5ace72ff89281",
+    "url": "/WeatherApp/static/js/main.699df228.chunk.js"
+  },
+  {
+    "revision": "277e07b228c9cfe3dd98",
+    "url": "/WeatherApp/static/js/runtime-main.127fcc36.js"
+  },
+  {
+    "revision": "1b0000ba0bd802b41a7cfbe22f6b7246",
+    "url": "/WeatherApp/static/media/aurora.1b0000ba.jpg"
+  }
+]);
